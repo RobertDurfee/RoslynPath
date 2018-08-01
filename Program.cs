d3dc53c393a8ed7c4920e1ad82ae1a3ff06f9f78
Robert Durfee <robert.durfee.17@gmail.com>
2018-07-31 21:59:54 -0500

    Sketched out VERY messy details for recursive child search.

--- Program.cs
+++ Program.cs
@@ -1,32 +1,102 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace RoslynPath
 {
+    class MatchTreeNode
+    {
+        public MatchTreeNode Parent { get; }
+        
+        public SyntaxNode SyntaxNode { get; }
+        
+        public List<MatchTreeNode> Children { get; } = new List<MatchTreeNode>();
+
+        public MatchTreeNode(MatchTreeNode parent, SyntaxNode syntaxNode)
+        {
+            Parent = parent;
+            SyntaxNode = syntaxNode;
+        }
+    }
+
+    class MatchPathNode
+    {
+        public MatchPathNode Parent { get; set; }
+
+        public Regex Pattern { get; set; }
+
+        public MatchPathNode Child { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            SyntaxTree tree = CSharpSyntaxTree.ParseText(@"
-            using System;
+            SyntaxNode sourceRoot = CSharpSyntaxTree.ParseText(File.ReadAllText("Test.cs")).GetRoot();
 
-            namespace Test
+            //(?s:.)+?(?<brack>\{(?>[^{}]|(?&brack))*\})
+
+            MatchPathNode linkA = new MatchPathNode()
             {
-                class Program
-                {
-                    static void Main(string[] args)
-                    {
-                        Console.WriteLine(""This is a test"");
-                    }
-                }
-            }");
+                Pattern = new Regex(@"namespace Test")
+            };
+            MatchPathNode linkB = new MatchPathNode()
+            {
+                Pattern = new Regex(@"class Program.")
+            };
+            MatchPathNode linkC = new MatchPathNode()
+            {
+                Pattern = new Regex(@"static void Method.")
+            };
+            MatchPathNode linkD = new MatchPathNode()
+            {
+                Pattern = new Regex(@"(?s).+")
+            };
+            MatchPathNode linkE = new MatchPathNode()
+            {
+                Pattern = new Regex(@"Console\.WriteLine\(.+?\)")
+            };
 
-            SyntaxNode root = tree.GetRoot();
+            linkA.Child = linkB;
+            linkB.Parent = linkA;
 
-            IEnumerable<MemberDeclarationSyntax> members = root.DescendantNodes().OfType<MemberDeclarationSyntax>();
+            linkB.Child = linkC;
+            linkC.Parent = linkB;
+
+            linkC.Child = linkD;
+            linkD.Parent = linkC;
+
+            linkD.Child = linkE;
+            linkE.Parent = linkD;
+
+            MatchTreeNode matches = FindMatchingNodesRecursive(new MatchTreeNode(null, sourceRoot), linkA, File.ReadAllText("Test.cs"));
+        }
+
+        static MatchTreeNode FindMatchingNodesRecursive(MatchTreeNode parentMatchTreeNode, MatchPathNode matchPathNode, string sourceText)
+        {
+            if (matchPathNode == null)
+                return parentMatchTreeNode;
+
+            IEnumerable<TextSpan> matchingTextSpans = matchPathNode.Pattern.Matches(sourceText.Substring(parentMatchTreeNode.SyntaxNode.SpanStart, parentMatchTreeNode.SyntaxNode.Span.Length)).Cast<Match>().Select(m => new TextSpan(m.Index + parentMatchTreeNode.SyntaxNode.SpanStart, m.Length));
+
+            if (matchingTextSpans.Count() == 0)
+                return null;
+
+            foreach (TextSpan matchingTextSpan in matchingTextSpans)
+            {
+                SyntaxNode matchingNode = parentMatchTreeNode.SyntaxNode.ChildNodes().Where(dn => dn.Span.OverlapsWith(matchingTextSpan))
+                                                                                    .OrderBy(dn => Math.Abs(dn.Span.Length - (int)dn.Span.Overlap(matchingTextSpan)?.Length))
+                                                                                    .Last();
+
+                parentMatchTreeNode.Children.Add(FindMatchingNodesRecursive(new MatchTreeNode(parentMatchTreeNode, matchingNode), matchPathNode.Child, sourceText));
+            }
+
+            return parentMatchTreeNode;
         }
     }
 }
---
+++ Test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Test
+{
+    class Program1
+    {
+        static void Method1()
+        {
+            Console.WriteLine("This is a test");
+
+            Console.WriteLine("Random");
+        }
+        
+        static void Method2()
+        {
+            Console.WriteLine("This is a test");
+
+            void Test()
+            {
+                Console.WriteLine("This is a test");
+            }
+
+            Console.WriteLine("Random");
+        }
+    }
+
+    class Program2
+    {
+        static void Method1()
+        {
+            Console.WriteLine("This is a test");
+
+            Console.WriteLine("Random");
+        }
+
+        static void Method2()
+        {
+            Console.WriteLine("This is a test");
+
+            void Test()
+            {
+                Console.WriteLine("This is a test");
+            }
+
+            Console.WriteLine("Random");
+        }
+    }
+}
