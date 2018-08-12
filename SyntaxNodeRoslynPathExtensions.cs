3c08dd5787945bcb920fe0b91ecf47860194b5fa
Robert Durfee <robert.durfee.17@gmail.com>
2018-08-12 11:44:49 -0500

    Added casted generic

--- SyntaxNodeRoslynPathExtensions.cs
+++ SyntaxNodeRoslynPathExtensions.cs
@@ -6,8 +6,10 @@ namespace RoslynPath
 {
     public static class SyntaxNodeRoslynPathExtensions
     {
-        public static SyntaxNode SelectNode(this SyntaxNode syntaxNode, string path) => SelectNodes(syntaxNode, path).First();
+        public static T SelectNode<T>(this SyntaxNode syntaxNode, string path) where T : SyntaxNode => SelectNodes<T>(syntaxNode, path).FirstOrDefault();
+        public static IEnumerable<T> SelectNodes<T>(this SyntaxNode syntaxNode, string path) where T : SyntaxNode => SelectNodes(syntaxNode, path).Cast<T>();
 
+        public static SyntaxNode SelectNode(this SyntaxNode syntaxNode, string path) => SelectNodes(syntaxNode, path).FirstOrDefault();
         public static IEnumerable<SyntaxNode> SelectNodes(this SyntaxNode syntaxNode, string path)
         {
             IEnumerable<RPToken> tokens = new RPTokenizer().Tokenize(path);
